28d28e1a372bf925e3f26c15c1194b266ce2037e
Lara Matos Aguirres <lara.laramatos@gmail.com>
2024-01-09 10:22:55 -0300

    Props

--- frontendweb/src/components/ButtonIcon/index.tsx
+++ frontendweb/src/components/ButtonIcon/index.tsx
@@ -1,11 +1,15 @@
 import './style.css';
 import {ReactComponent as Seta} from '../../assets/img/Seta.svg';
 
-const ButtonIcon = () => {
+type Props = {
+  text: string;
+}
+
+const ButtonIcon = ({text}: Props) => {
   return (
     <div className="btn-container">
         <button className="btn bg-primary">
-          <h6> INICIE A SUA BUSCA AGORA</h6>
+          <h6> {text}</h6>
         </button>
       <div className="btn-icon-container">
         <Seta />
--- frontendweb/src/pages/Home/index.tsx
+++ frontendweb/src/pages/Home/index.tsx
@@ -14,7 +14,7 @@ const Home = () =>{
                 </div>
                 <div>
                   <Link to="/products">
-                  <ButtonIcon />
+                  <ButtonIcon text='Inicie a sua busca agora' />
                   </Link>
                 </div>
             </div>
